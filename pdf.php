4840ac271d459035a50e5c333134cc0c8432d586
spiuri <61064112+spiuri@users.noreply.github.com>
2021-11-09 13:53:25 -0300

    pdf salvando

--- pdf.php
+++ pdf.php
@@ -1,7 +1,13 @@
 
 <?php
+require_once 'vendor/autoload.php';
+use Dompdf\Dompdf;
+$conexao = mysqli_connect("localhost","root","","PurchasesDB");
 session_start();
 $id = $_SESSION['id'];
+$query = "SELECT id,description,amount, customer_id, insulina FROM orders WHERE customer_id =$id";
+
+$resultado = mysqli_query($conexao,$query);
 $name= $_SESSION['name'];
 $sangue = $_SESSION['sangue'];
 $genero = $_SESSION['genero'];
@@ -9,15 +15,31 @@ $nasc = $_SESSION['nasc'];
 $html = '<h3 style="text-align: center;">Histórico de glicose e insulina</h3><br><br>';
 $html .= '<strong>Nome: </strong>'.$name.'.<br>';
 $html .= '<strong>Tipo Sanguineo: </strong>'. $sangue.'.<br>';
-$html .= '<strong>Data de Nasc: </strong>'. $nasc.'.';
-$html .= '<strong>Gênero: </strong>'. $genero.'.';
+$html .= '<strong>Data de Nasc: </strong>'. $nasc.'.<br>';
+$html .= '<strong>Gênero: </strong>'. $genero.'.<br>';
+$html .='<table class="table">';
+$html .='<thead>';
+$html .='<tr>';
+    
+$html .='<th scope="col">Periodo</th>';
+$html .='<th scope="col">Glicemia</th>';
+$html .='<th scope="col">Insulina<th>';
+$html .='<th scope="col"><th>';
+$html .='<th scope="col"><th>';
 
-ss;
+$html .='</tr>';
+$html .='</thead>';
+$html .='<tbody>';
+$html .='<tr>';
+while($linha = mysqli_fetch_array($resultado)){
+$html.='<td>'. $linha['description'].'</td>';
+$html.='<td>'. $linha['amount'].'</td>';
+$html.='<td>'. $linha['insulina'].'</td></tr>';
 
+}
 
+$html.='</tbody></table>';
 
-require_once 'vendor/autoload.php';
-use Dompdf\Dompdf;
 
 $dompdf =new Dompdf;
 
