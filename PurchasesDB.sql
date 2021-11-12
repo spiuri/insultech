18827c03ac35e6445d9dfdd98ab198380347616d
spiuri <61064112+spiuri@users.noreply.github.com>
2021-11-11 21:55:32 -0300

    Update PurchasesDB.sql

--- PurchasesDB.sql
+++ PurchasesDB.sql
@@ -1,21 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12-Nov-2021 às 01:54
+-- Versão do servidor: 10.4.20-MariaDB
+-- versão do PHP: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `purchasesdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `customers`
+--
+
 CREATE TABLE `customers` (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  `name` VARCHAR( 255 ) NOT NULL,
-  `email` VARCHAR( 255 ) NOT NULL,
-  `passwd` VARCHAR( 255 ) NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `passwd` varchar(255) NOT NULL,
+  `sangue` varchar(3) DEFAULT NULL,
+  `genero` varchar(45) DEFAULT NULL,
+  `nasc` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `customers`
+--
 
-)
- ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `customers` (`id`, `name`, `email`, `passwd`, `sangue`, `genero`, `nasc`) VALUES
+(9, 'Iuri Matos', '123@gmail.com', '202cb962ac59075b964b07152d234b70', 'AB+', 'Masculino', '2014-01-08'),
+(10, 'Iuri Matos', 'admin@gmail.com', '202cb962ac59075b964b07152d234b70', 'O+', 'Prefiro não informar', '2022-01-10'),
+(11, 'Iuri Trajano de Matos', 'spiuri123@gmail.com', '202cb962ac59075b964b07152d234b70', 'A+', 'Prefiro não informar', '2021-11-19'),
+(12, 'Arthur lanches', 'iuri12354@gmail.com', '65ded5353c5ee48d0b7d48c591b8f430', 'O-', 'Feminino', '2012-01-12'),
+(13, 'Iuri Trajano de Matos', 'iuri12354@gmail.com', '202cb962ac59075b964b07152d234b70', 'A+', 'Masculino', '2021-11-10');
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `orders`
+--
 
 CREATE TABLE `orders` (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  `description` VARCHAR( 255 ) NOT NULL,
-  `amount` DOUBLE NOT NULL,
-  `customer_id` INT NOT NULL 
+  `id` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `amount` double NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `insulina` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-)
- ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Extraindo dados da tabela `orders`
+--
 
- ALTER TABLE `orders` ADD CONSTRAINT `fk_id_order` FOREIGN KEY ( `customer_id` ) REFERENCES `customers` ( `id` ) ;
+INSERT INTO `orders` (`id`, `description`, `amount`, `customer_id`, `insulina`) VALUES
+(32, 'Manha', 150, 10, 8);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_id_order` (`customer_id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de tabela `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_id_order` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
